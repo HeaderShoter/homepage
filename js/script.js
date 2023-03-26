a8abb18438674e0650a0d2b72065b4276682fb91
HeaderShoter <34886197+HeaderShoter@users.noreply.github.com>
2023-03-26 17:15:10 +0200

    tryb ciemny alfa działa

--- js/script.js
+++ js/script.js
@@ -2,9 +2,20 @@ console.log("Hellow world!");
 
 let button = document.querySelector(".bulbSwitch");
 let bulb = document.querySelector(".bulbImage");
+let brightElement = document.getElementById("bright");
 
 button.addEventListener("click", () => {
-  bulb.remove();
+  if (button.innerText === "ZGAŚ") {
+    button.innerText = "ZAPAL";
+    body.classList = "dark";
+    bulb.style.display = "none";
+  } else {
+    button.innerText = "ZGAŚ";
+    body.classList = "";
+    bulb.style.display = "block";
+  }
 });
+// bulb.remove();
+//});
 
 console.log(button);
