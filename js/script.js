dde3639ff11d04894475807ce0cad0c74632c4e2
Wiktor Żarski <wiktorzarski.kontakt@gmail.com>
2023-11-15 10:11:05 +0100

    JS functions rebuilt with const and simplyfied

--- js/script.js
+++ js/script.js
@@ -1,23 +1,36 @@
 console.log("Hello world!");
 
-let toggle = document.querySelector(".js-toggle__button");
-let body = document.querySelector(".js-body");
-let articleHeader = document.querySelector(".js-theme");
-let tableHeader = document.querySelector(".js-table__header");
-let bulbImage = document.querySelector(".js-image");
+const toggle = document.querySelector(".js-toggle__button");
+const body = document.querySelector(".js-body");
+const articleHeader = document.querySelector(".js-theme");
+const tableHeader = document.querySelector(".js-table__header");
+const bulbImage = document.querySelector(".js-image");
 
-toggle.addEventListener("click", () => {
+const toggleBackground = () => {
   body.classList.toggle("body--dark");
   articleHeader.classList.toggle("cssTheme--dark"); // It changes article headers body when light is off but for now it works only with just one of headers and I will fix this when I will know how to.
   tableHeader.classList.toggle("table__header--dark"); // It changes table headers body when light is off but for now it works only with just one of headers and I will fix this when I will know how to.
-  if (toggle.innerText === "ZGAŚ") {
-    toggle.innerText = "ZAPAL";
-    bulbImage.style.display = "none";
+};
+
+const darkMode = () => {
+  const toggle = document.querySelector(".js-toggle__button");
+  const bulbImage = document.querySelector(".js-image");
+  toggle.innerText = "ZAPAL";
+  bulbImage.style.display = "none";
+};
+
+const brightMode = () => {
+  const toggle = document.querySelector(".js-toggle__button");
+  const bulbImage = document.querySelector(".js-image");
+  toggle.innerText = "ZGAŚ";
+  bulbImage.style.display = "block";
+};
+
+toggle.addEventListener("click", () => {
+  toggleBackground();
+  toggle.innerText === "ZGAŚ" ? darkMode() : brightMode();
   }
-  else {
-    toggle.innerText = "ZGAŚ";
-    bulbImage.style.display = "block";
-  }
-});
+);
 
 console.log(button);
+
