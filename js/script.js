ba43760d74cadc6fc0ea92988d427c369156142b
Wiktor Żarski <wiktorzarski.kontakt@gmail.com>
2023-11-16 17:34:18 +0100

    JavaScript overcutted into three simple functions

--- js/script.js
+++ js/script.js
@@ -3,11 +3,21 @@ console.log("Hello world!");
 const themeToggle = document.querySelector(".js-themeToggle");
 
 themeToggle.addEventListener("click", () => {
+  toggleButtonText();
   toggleBackground();
-  themeToggle.innerText === "ZGAŚ" ? darkMode() : brightMode();
   bulbImageShow();
 }
 );
+
+const toggleButtonText = () => {
+  switch (themeToggle.innerText) {
+    case "ZGAŚ": themeToggle.innerText = "ZAPAL";
+      window.darkModeStatus = 'on';
+      break;
+    default: themeToggle.innerText = "ZGAŚ";
+      window.darkModeStatus = 'off';
+  }
+};
 
 const toggleBackground = () => {
   const body = document.querySelector(".js-body");
@@ -16,16 +26,6 @@ const toggleBackground = () => {
   body.classList.toggle("body--dark");
   articleHeader.classList.toggle("cssTheme--dark"); // It changes article headers body when light is off but for now it works only with just one of headers and I will fix this when I will know how to.
   tableHeader.classList.toggle("table__header--dark"); // It changes table headers body when light is off but for now it works only with just one of headers and I will fix this when I will know how to.
-};
-
-const darkMode = () => {
-  themeToggle.innerText = "ZAPAL";
-  window.darkModeStatus = 'on';
-};
-
-const brightMode = () => {
-  themeToggle.innerText = "ZGAŚ";
-  window.darkModeStatus = 'off';
 };
 
 const bulbImageShow = () => {
