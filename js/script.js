586e559dbc0ba5ca68aec287fccf69df77b7698e
Wiktor Żarski <wiktorzarski.kontakt@gmail.com>
2023-11-17 09:57:33 +0100

    functions moved on the top

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 console.log("Hello world!");
+{
+  const themeToggle = document.querySelector(".js-themeToggle");
 
-const themeToggle = document.querySelector(".js-themeToggle");
-
-themeToggle.addEventListener("click", () => {
-  toggleButtonText();
-  toggleBackground();
-  bulbImageShow();
-}
-);
-
-const toggleButtonText = () => {
-  if (themeToggle.innerText === "ZGAŚ") {
-    themeToggle.innerText = "ZAPAL";
-    window.darkModeStatus = 'on';
-  } else {
-    themeToggle.innerText = "ZGAŚ";
-    window.darkModeStatus = 'off';
+  const toggleButtonText = () => {
+    if (themeToggle.innerText === "ZGAŚ") {
+      themeToggle.innerText = "ZAPAL";
+      window.darkModeStatus = 'on';
+    } else {
+      themeToggle.innerText = "ZGAŚ";
+      window.darkModeStatus = 'off';
+    };
   };
-};
 
-const toggleBackground = () => {
-  const body = document.querySelector(".js-body");
-  const articleHeader = document.querySelector(".js-theme");
-  const tableHeader = document.querySelector(".js-table__header");
-  body.classList.toggle("body--dark");
-  articleHeader.classList.toggle("cssTheme--dark"); // It changes article headers body when light is off but for now it works only with just one of headers and I will fix this when I will know how to.
-  tableHeader.classList.toggle("table__header--dark"); // It changes table headers body when light is off but for now it works only with just one of headers and I will fix this when I will know how to.
-};
+  const toggleBackground = () => {
+    const body = document.querySelector(".js-body");
+    const articleHeader = document.querySelector(".js-theme");
+    const tableHeader = document.querySelector(".js-table__header");
+    body.classList.toggle("body--dark");
+    articleHeader.classList.toggle("cssTheme--dark"); // It changes article headers body when light is off but for now it works only with just one of headers and I will fix this when I will know how to.
+    tableHeader.classList.toggle("table__header--dark"); // It changes table headers body when light is off but for now it works only with just one of headers and I will fix this when I will know how to.
+  };
 
-const bulbImageShow = () => {
-  const bulbImage = document.querySelector(".js-image");
-  darkModeStatus === 'off' ? bulbImage.style.display = "block" : bulbImage.style.display = "none";
-};
+  const bulbImageShow = () => {
+    const bulbImage = document.querySelector(".js-image");
+    darkModeStatus === 'off' ? bulbImage.style.display = "block" : bulbImage.style.display = "none";
+  };
+
+  themeToggle.addEventListener("click", () => {
+    toggleButtonText();
+    toggleBackground();
+    bulbImageShow();
+  });
+}
